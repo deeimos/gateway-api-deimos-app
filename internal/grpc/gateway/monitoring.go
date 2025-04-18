81b0e2550777f2d9aaf7cc8af2e585b9c2ce203c
deimos <dmsnellen@gmail.com>
2025-04-18 18:14:23 +0300

    add stream monitoring grpc handler

--- internal/grpc/gateway/monitoring.go
+++ internal/grpc/gateway/monitoring.go
@@ -2,29 +2,62 @@ package gateway
 
 import (
 	"context"
+	"gateway-api/internal/middleware"
 
 	gateway_apiv1 "github.com/deeimos/proto-deimos-app/gen/go/public-api"
 	servers_apiv1 "github.com/deeimos/proto-deimos-app/gen/go/servers-api"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type Monitoring interface {
 	StreamServerMetrics(ctx context.Context, serverID, userID string, stream servers_apiv1.ServersAPI_StreamServerMetricsServer) error
 }
 
-func (s *serverApi) StreamServerMetrics(req *gateway_apiv1.PublicServerMetricsRequest, stream gateway_apiv1.PublicMonitoring_StreamServerMetricsServer) error {
-	// if req.GetServerId() == "" {
-	// 	return status.Error(codes.InvalidArgument, "Отсутствует ID сервера")
-	// }
+// streamAdapter реализует servers_apiv1.ServersAPI_StreamServerMetricsServer
+type streamAdapter struct {
+	gateway_apiv1.PublicMonitoring_StreamServerMetricsServer
+}
 
-	// if req.GetToken() == "" {
-	// 	return status.Error(codes.Unauthenticated, "Отсутствует токен")
-	// }
+func (a *streamAdapter) Send(metric *servers_apiv1.ServerMetric) error {
+	return a.PublicMonitoring_StreamServerMetricsServer.Send(&gateway_apiv1.PublicServerMetric{
+		CpuUsage:      metric.CpuUsage,
+		MemoryUsage:   metric.MemoryUsage,
+		DiskUsage:     metric.DiskUsage,
+		LoadAvg_1:     metric.LoadAvg_1,
+		LoadAvg_5:     metric.LoadAvg_5,
+		LoadAvg_15:    metric.LoadAvg_15,
+		NetworkRx:     metric.NetworkRx,
+		NetworkTx:     metric.NetworkTx,
+		DiskRead:      metric.DiskRead,
+		DiskWrite:     metric.DiskWrite,
+		ProcessCount:  metric.ProcessCount,
+		IoWait:        metric.IoWait,
+		UptimeSeconds: metric.UptimeSeconds,
+		Temperature:   metric.Temperature,
+		Status:        metric.Status,
+		Timestamp:     metric.Timestamp,
+	})
+}
 
-	// userID, err := s.auth.ValidateAccessToken(req.GetToken())
-	// if err != nil {
-	// 	return status.Error(codes.Unauthenticated, "Неверный или недействительный токен")
-	// }
+func (s *serverApi) StreamServerMetrics(
+	req *gateway_apiv1.PublicServerMetricsRequest,
+	stream gateway_apiv1.PublicMonitoring_StreamServerMetricsServer,
+) error {
+	userID, ok := stream.Context().Value(middleware.UserIDKey).(string)
+	if !ok {
+		return status.Error(codes.Internal, "userId is undefined")
+	}
 
-	// return s.servers.StreamServerMetrics(stream.Context(), req.GetServerId(), userID, stream)
-	panic("implement me")
+	err := s.monitoring.StreamServerMetrics(
+		stream.Context(),
+		req.GetServerId(),
+		userID,
+		&streamAdapter{stream},
+	)
+	if err != nil {
+		return s.errMapper.HandleGRPC(err)
+	}
+
+	return nil
 }
